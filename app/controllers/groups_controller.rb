f23d0922ddb0d7e9aa059d4f9a5ce0baa89cdd7c
two_app <wangyuliangf@gmail.com>
2017-03-23 11:54:46 +0800

    wen zi xiu gai

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -80,6 +80,6 @@ class GroupsController < ApplicationController
   end
 
   def group_params
-    params.require(:group).permit(:title, :description)
+    params.require(:group).permit(:title, :description, :image)
   end
 end
